3a94fc43b77346140e56b6450059f83bbf379c18
KeepOnKeepOn <longwudi@qq.com>
2023-01-11 21:33:48 +0800

    add path subscribing and printing to csv

--- src/test.cpp
+++ src/test.cpp
@@ -1,17 +1,28 @@
 #include<fstream>
 #include<string>
 #include<iostream>
+#include<thread>
+#include<nav_msgs/Path.h>
 #include<geometry_msgs/TransformStamped.h>
 #include<tf2_ros/transform_broadcaster.h>
 using namespace std;
-
-int main(int argc,char **argv)
-{
-    ros::init(argc,argv,"test"); 
-    ros::NodeHandle nh("~");
-    string csv_path;
-    nh.param<string>("csv_path",csv_path,"./");
-    shared_ptr<std::ifstream> in_file = make_shared<std::ifstream>(csv_path+"pose.csv", std::ios::in );
+string csv_path;
+shared_ptr<std::ifstream> in_file;
+void pathCallback(nav_msgs::Path::ConstPtr msg){
+    std::ofstream out_file(csv_path+"pose_out.csv", std::ios::trunc | std::ios::out );
+    out_file.precision(20);
+    for(auto pose_stamped:msg->poses){
+        double timestamp = pose_stamped.header.stamp.toSec();
+        stringstream ss;
+        ss.setf(std::ios::fixed);
+        ss.precision(20);
+        ss<<timestamp;
+        out_file<<std::to_string(timestamp)<<" "<<pose_stamped.pose.position.x<<" "<<pose_stamped.pose.position.y<<" "<<pose_stamped.pose.position.z<<" "<<
+        pose_stamped.pose.orientation.x<<" "<<pose_stamped.pose.orientation.y<<" "<<pose_stamped.pose.orientation.z<<" "<<pose_stamped.pose.orientation.w<<endl;
+    }  
+    out_file.close();
+}
+void readPose(){
     geometry_msgs::TransformStamped transform;
     transform.header.frame_id = "world";
     transform.child_frame_id = "camera_link";
@@ -42,7 +53,34 @@ int main(int argc,char **argv)
         broadcaster.sendTransform(transform);
         r.sleep();
     }
+}
+int main(int argc,char **argv)
+{
+    ros::init(argc,argv,"test"); 
+    ros::NodeHandle nh("~");
+    string path_topic;
+    ros::Subscriber path_sub;
+    std::shared_ptr<std::thread> read_thread;
+    bool read_pose_from_csv, print_pose_from_path;
+    nh.param<string>("csv_path", csv_path, "./");
+    nh.param<string>("path_topic", path_topic, "/path");
+    nh.param<bool>("read_pose_from_csv",read_pose_from_csv,false);
+    nh.param<bool>("print_pose_from_path",print_pose_from_path,false);
+    if(print_pose_from_path){
+        path_sub = nh.subscribe(path_topic, 10000, &pathCallback);
+    }
+    if(read_pose_from_csv){
+        in_file = make_shared<std::ifstream>(csv_path+"pose_in.csv", std::ios::in );  
+        read_thread = make_shared<std::thread>(&readPose);
+    } 
 
+    // ros::Rate rate(10);
+    // while(ros::ok()){
+    //     ros::spinOnce();
+    //     rate.sleep();
+    // }
+    ros::spin();
+    read_thread->join();
     ros::shutdown();
     return 0;
 }
